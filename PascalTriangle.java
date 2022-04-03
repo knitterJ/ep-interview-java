dfe802897175373b00d68307b9618c1ff53e5bc8
knitterJ <knitter.m.jacob@gmail.com>
2022-04-04 00:06:17 +0200

    coding challenge daily D64

---
+++ PascalTriangle.java
@@ -0,0 +1,18 @@
+// This method should draw Pascal Triangle. How to construct it?
+// Topmost row has one element, ...
+public class PascalTriangle {
+
+  public static int pascalTriangle(int numberOfRows){
+
+     int[] previousRow = new int[]{1};
+     for (int row = 1; row < numberOfRows; row++){
+
+           System.out.println("efwjlkaf");
+     }
+     return 10;
+  }
+
+  public static void main (String args[]){
+    System.out.println(pascalTriangle(12));
+  }
+}
