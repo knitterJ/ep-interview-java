3c4b8a5220c36f6440a56bc900e2fd516a02ff9f
knitterJ <knitter.m.jacob@gmail.com>
2022-05-22 15:33:13 +0200

    c

--- 100-questions/1.override-private/Child.java
+++ 100-questions/1.override-private/Child.java
@@ -9,13 +9,17 @@ public class Child extends Base {
   }
 
 
-
-  
   public static void main(String[] args) {
     Base stdObj = new Child();
     stdObj.nothingSpecialMethod();
 
     // error: impossiblePart() has private access in Base
     // stdObj.impossiblePart();
+
+    //Calling method this way also won't work! You'd have to use Java reflection in order to get to this method from Child class 
+    // Base noOtherChoiceObj = new Base();
+    // noOtherChoiceObj.impossiblePart();
+
+    //
   }
 }
