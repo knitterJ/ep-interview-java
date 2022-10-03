87b95dbc3aa9cb41d96e780167cee3a98e9c9e7e
knitterJ <knitter.m.jacob@gmail.com>
2022-10-03 14:02:25 +0200

    coding challenge daily

---
+++ spring-thanks-b/backend-stateless-jwt-authentication/src/main/java/com/example/backendstatelessjwtauthentication/BackendStatelessJwtAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendstatelessjwtauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStatelessJwtAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStatelessJwtAuthenticationApplication.class, args);
+    }
+
+}
---
+++ spring-thanks-b/backend-stateless-jwt-authentication/src/test/java/com/example/backendstatelessjwtauthentication/BackendStatelessJwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendstatelessjwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStatelessJwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
